f8db9260a6513fa46bfc74d8cd784d58519624d3
Jesus Rodriguez <98504031+jrodriguez14@users.noreply.github.com>
2022-10-24 15:04:21 -0500

    Update streamlit_app.py
    added comment further describing fruit_choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display API response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')#kiwi is entered as default value
 streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
