64c4f448755c62f7f01aceed79234fcb2944a12f
Jesus Rodriguez <98504031+jrodriguez14@users.noreply.github.com>
2022-10-24 17:44:27 -0500

    Update streamlit_app.py
    fixed a typo, added a )

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ except URLError as e:
 #import snowflake.connector
 
 #querying snowflake account metadata
-streamlit.header("The fruit load list contains:"
+streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
